382167b6c47781a4afa08a04a43f0f3fd12d703e
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2026-01-27 18:04:46 +0100

    Added validator inside chat hub

--- backend/Api/Hubs/ChatHub.cs
+++ backend/Api/Hubs/ChatHub.cs
@@ -1,12 +1,14 @@
 using backend.Dto.Messages.Request;
 using backend.Services.MessageService;
+using FluentValidation;
 using Microsoft.AspNetCore.SignalR;
 
 namespace backend.Hubs;
 
-public class ChatHub(IMessageService messageService) : Hub
+public class ChatHub(IMessageService messageService, IValidator<MessageRequest> messageReqValidator) : Hub
 {
     private readonly IMessageService _messageService = messageService;
+    private readonly IValidator<MessageRequest> _messageReqValidator = messageReqValidator;
 
     public async Task JoinRoom(string roomId)
     {
@@ -15,6 +17,13 @@ public class ChatHub(IMessageService messageService) : Hub
 
     public async Task SendMessage(MessageRequest request)
     {
+        var validationResult = await _messageReqValidator.ValidateAsync(request);
+        if (!validationResult.IsValid)
+        {
+            await Clients.Caller.SendAsync("ValidationFailed", validationResult.Errors);
+            return;
+        }
+
         // Firstly, save this message inside db
         var response = await _messageService.SendMessage(request);
 
