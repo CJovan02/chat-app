c1e4ad612cd29b0228a7b4a7f2e357609e4ca50f
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2026-01-27 16:41:26 +0100

    Added global exception handler

--- backend/Api/Services/MessageService/IMessageService.cs
+++ backend/Api/Services/MessageService/IMessageService.cs
@@ -1,11 +1,12 @@
 using backend.Dto.Messages.Request;
 using backend.Dto.Messages.Response;
+using backend.ResultPattern;
 
 namespace backend.Services.MessageService;
 
 public interface IMessageService
 {
-    Task<MessageResponse> SendMessage(MessageRequest request);
+    Task<Result<MessageResponse>> SendMessage(MessageRequest request);
 
-    Task<IEnumerable<MessageResponse>> GetMessagesAsync(string roomId, int pageSize, string? beforeId = null);
+    Task<Result<IEnumerable<MessageResponse>>> GetMessagesAsync(string roomId, int pageSize, string? beforeId = null);
 }
