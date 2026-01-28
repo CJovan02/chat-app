288571f7237cd5422549c165073e82ee9cba256b
Aleksa Perić <alex.peric1205@gmail.com>
2026-01-28 01:45:14 +0100

    Changed config

--- frontend/vite.main.config.ts
+++ frontend/vite.main.config.ts
@@ -1,14 +1,19 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config
 export default defineConfig({
   build: {
-    outDir: 'dist/main',
+    outDir: '.vite/build',
+    lib: {
+      entry: './src/main.ts',
+      fileName: () => 'main',
+      formats: ['cjs'],
+    },
     rollupOptions: {
-      input: './src/main.ts',
+      external: ['electron'],
+      output: {
+        entryFileNames: 'main.cjs',
+      },
     },
   },
   resolve: {
