c72c60ed8b81fef5eb275b5cf24126f90c729045
Aleksa Perić <alex.peric1205@gmail.com>
2026-01-30 11:54:44 +0100

    Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,7 +9,7 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { Label } from '@radix-ui/react-label';
+import { Label } from '@/components/ui/label';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
